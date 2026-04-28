941810c03819a1784bef1649afe7f3bc9ca5987a
stcrex <stcrex1@gmail.com>
2026-04-28 21:42:29 +0800

    Added threshold comparison and plotting

--- pre_trained_model/Transformer/infer.py
+++ pre_trained_model/Transformer/infer.py
@@ -30,16 +30,66 @@ if __name__ == "__main__":
 
     errors, labels = compute_errors(model, loader, device)
 
-    threshold = np.percentile(errors, 41)
-    preds = (errors > threshold).astype(int)
 
-    print("Threshold:", threshold)
+    # thresholds = np.linspace(errors.min(), errors.max(), 100)
+    low = np.percentile(errors, 0)
+    high = np.percentile(errors, 70)#train on test:66
 
-    if labels is not None:
-        from sklearn.metrics import precision_score, recall_score, f1_score
+    thresholds = np.linspace(low, high, 100)
 
-        print("Precision:", precision_score(labels, preds))
-        print("Recall:", recall_score(labels, preds))
-        print("F1:", f1_score(labels, preds))
-    else:
-        print("No labels, only anomaly scores computed.")
+    precisions = []
+    recalls = []
+    f1s = []
+    accs = []
+
+    from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+
+    for th in thresholds:
+        preds = (errors > th).astype(int)
+
+        precisions.append(precision_score(labels, preds))
+        recalls.append(recall_score(labels, preds))
+        f1s.append(f1_score(labels, preds))
+        accs.append(accuracy_score(labels, preds))
+
+    # best_idx = np.argmax(f1s)
+    best_idx = np.argmax(recalls)
+    best_th = thresholds[best_idx]
+
+    print("Best Threshold:", best_th)
+    print("Best Precision:", precisions[best_idx])
+    print("Best Recall:", recalls[best_idx])
+    print("Best F1:", f1s[best_idx])
+    print("Best Accuracy:", accs[best_idx])
+
+    import matplotlib.pyplot as plt
+
+    plt.figure()
+    plt.plot(thresholds, f1s, label="F1")
+    plt.plot(thresholds, recalls, label="Recall")
+    plt.plot(thresholds, precisions, label="Precision")
+    plt.plot(thresholds, accs, label="Accuracy")
+
+    # best threshold
+    plt.axvline(best_th, linestyle="--", label="Best Threshold")
+
+    plt.xlabel("Threshold")
+    plt.ylabel("Score")
+    plt.title("Performance vs Threshold")
+    plt.legend()
+    plt.grid()
+
+    plt.show()
+    # threshold = np.percentile(errors, 41)
+    # preds = (errors > threshold).astype(int)
+
+    # print("Threshold:", threshold)
+
+    # if labels is not None:
+    #     from sklearn.metrics import precision_score, recall_score, f1_score
+
+    #     print("Precision:", precision_score(labels, preds))
+    #     print("Recall:", recall_score(labels, preds))
+    #     print("F1:", f1_score(labels, preds))
+    # else:
+    #     print("No labels, only anomaly scores computed.")
--- pre_trained_model/Transformer/readme.txt
+++
@@ -1,4 +0,0 @@
-conda create -n myenv python=3.13
-pip install torch pandas numpy scikit-learn
-python train.py
-python infer.py
